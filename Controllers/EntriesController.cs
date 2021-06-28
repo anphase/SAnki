24dd06b8ee33163317cc95da8b8d5b483f57113d
anphase <panashemn@gmail.com>
2021-06-28 22:53:59 +1200

    first commit

---
+++ Controllers/EntriesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SAnki.Data;
+using SAnki.Models;
+
+namespace SAnki.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EntriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Entries
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Entry.ToListAsync());
+        }
+
+        // GET: Entries/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            // No need to specify string name "ShowSearchForm" in View
+            return View();
+        }
+
+        // POST: Entries/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(string SearchPhrase)
+        {
+            // No need to specify string name "ShowSearchForm" in View
+            return View("Index", await _context.Entry.Where( q => q.Question.Contains(SearchPhrase)).ToListAsync());
+        }
+
+        // GET: Entries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entry
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
+        }
+
+        // GET: Entries/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Entries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Question,Answer")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(entry);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(entry);
+        }
+
+        // GET: Entries/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entry.FindAsync(id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Question,Answer")] Entry entry)
+        {
+            if (id != entry.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(entry);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EntryExists(entry.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(entry);
+        }
+
+        // GET: Entries/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entry
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
+        }
+
+        // POST: Entries/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var entry = await _context.Entry.FindAsync(id);
+            _context.Entry.Remove(entry);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EntryExists(int id)
+        {
+            return _context.Entry.Any(e => e.id == id);
+        }
+    }
+}
---
+++ Data/Migrations/20210628041132_initial migration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SAnki.Data.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entry",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entry", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entry");
+        }
+    }
+}
